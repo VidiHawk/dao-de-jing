8455e740cc0dac5ff6c83fc848df332cc4dc8b93
VidiHawk <fabian@demortier.net>
2022-06-13 15:33:20 +0200

    removed duplicate dir

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Dao de Jing
+
+### Next steps
+
+- make it a PWA
+- should be able to use it offline
+
+### References
+
+https://codepen.io/OlgaKoplik/pen/pXGQNQ
+
+https://codepen.io/rutcoba/pen/jOOGxZz
--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./App.scss";
+import Player from "./components/Player";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Player />
+    </div>
+  );
 }
 
 export default App;
