6786ff2d56bf18b6f2143a7a3a7dfcc1b6d6a493
VidiHawk <fabian@demortier.net>
2022-06-24 16:19:21 +0200

    moved const audioDurations and const audioList to AudioList.js

---
+++ src/utils/AudioList.js
@@ -0,0 +1,167 @@
+const audioDurations = [
+  "00:14",
+  "00:34",
+  "00:45",
+  "00:34",
+  "00:23",
+  "00:25",
+  "00:15",
+  "00:25",
+  "00:30",
+  "00:22",
+  "00:36",
+  "00:26",
+  "00:26",
+  "00:40",
+  "00:45",
+  "00:46",
+  "00:37",
+  "00:26",
+  "00:18",
+  "00:24",
+  "01:05",
+  "00:36",
+  "00:40",
+  "00:41",
+  "00:25",
+  "00:43",
+  "00:24",
+  "00:44",
+  "00:46",
+  "00:33",
+  "00:37",
+];
+
+const AudioList = [
+  {
+    name: "道德经",
+    duration: audioDurations[0],
+  },
+  {
+    name: "第一章",
+    duration: audioDurations[1],
+  },
+  {
+    name: "第二章",
+    duration: audioDurations[2],
+  },
+  {
+    name: "第三章",
+    duration: audioDurations[3],
+  },
+  {
+    name: "第四章",
+    duration: audioDurations[4],
+  },
+  {
+    name: "第五章",
+    duration: audioDurations[5],
+  },
+  {
+    name: "第六章",
+    duration: audioDurations[6],
+  },
+  {
+    name: "第七章",
+    duration: audioDurations[7],
+  },
+  {
+    name: "第八章",
+    duration: audioDurations[8],
+  },
+  {
+    name: "第九章",
+    duration: audioDurations[9],
+  },
+  {
+    name: "第十章",
+    duration: audioDurations[10],
+  },
+  {
+    name: "第十一章",
+    duration: audioDurations[11],
+  },
+  {
+    name: "第十二章",
+    duration: audioDurations[12],
+  },
+  {
+    name: "第十三章",
+    duration: audioDurations[13],
+  },
+  {
+    name: "第十四章",
+    duration: audioDurations[14],
+  },
+  {
+    name: "第十五章",
+    duration: audioDurations[15],
+  },
+  {
+    name: "第十六章",
+    duration: audioDurations[16],
+  },
+  {
+    name: "第十七章",
+    duration: audioDurations[17],
+  },
+  {
+    name: "第十八章",
+    duration: audioDurations[18],
+  },
+  {
+    name: "第十九章",
+    duration: audioDurations[19],
+  },
+  {
+    name: "第二十章",
+    duration: audioDurations[20],
+  },
+  {
+    name: "第二十一章",
+    duration: audioDurations[21],
+  },
+  {
+    name: "第二十二章",
+    duration: audioDurations[22],
+  },
+  {
+    name: "第二十三章",
+    duration: audioDurations[23],
+  },
+  {
+    name: "第二十四章",
+    duration: audioDurations[24],
+  },
+  {
+    name: "第二十五章",
+    duration: audioDurations[25],
+  },
+  {
+    name: "第二十六章",
+    duration: audioDurations[26],
+  },
+  {
+    name: "第二十七章",
+    duration: audioDurations[27],
+  },
+  {
+    name: "第二十八章",
+    duration: audioDurations[28],
+  },
+  {
+    name: "第二十九章",
+    duration: audioDurations[29],
+  },
+  {
+    name: "第三十章",
+    duration: audioDurations[30],
+  },
+  {
+    name: "214个部首",
+  },
+  {
+    name: "100个部首",
+  },
+];
+export default AudioList;
